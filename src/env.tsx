25ff39faf0598c5d8b98b00913797741462f2f8e
francesco-orlando9 <francesco.orlando.dev@gmail.com>
2023-09-09 22:29:23 +0200

    add env var file

---
+++ src/env.tsx
@@ -0,0 +1,6 @@
+// qua bisogna fare in modo di passare la lingua ed ottenere la url con la lingua
+// en="en_US" - it="it_IT"
+export const ENV = {
+  url: `https://ddragon.leagueoflegends.com/cdn/13.17.1/data/en_US/champion.json`,
+  url2: "https://ddragon.leagueoflegends.com/cdn/13.17.1/data/en_US/champion/Aatrox.json",
+};
