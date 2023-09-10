cfeb76ffda97b2e95347b2cb25fa0a58578d54df
francesco-orlando9 <francesco.orlando.dev@gmail.com>
2023-09-10 18:32:41 +0200

    add url utils

---
+++ src/utils/urlUtils.tsx
@@ -0,0 +1,21 @@
+import { ENV } from "../env";
+
+export function getChampionsUrl() {
+  const { baseUrl } = ENV;
+  const langCode = navigator.language === "it" ? "it_IT" : "en_US";
+
+  return `${baseUrl}/13.17.1/data/${langCode}/champion.json`;
+}
+
+export function getChampionUrl(championName: string) {
+  const { baseUrl } = ENV;
+  const langCode = navigator.language === "it" ? "it_IT" : "en_US";
+
+  return `${baseUrl}/13.17.1/data/${langCode}/champion/${championName}.json`;
+}
+
+export function getChampionCardImgUrl(championName: string) {
+  const { baseUrl } = ENV;
+
+  return `${baseUrl}/img/champion/loading/${championName}_0.jpg`;
+}
