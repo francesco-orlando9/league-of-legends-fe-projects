ed39a3e79eaf2f0d7bf78695c0d33e38f7389ba2
francesco-orlando9 <francesco.orlando.dev@gmail.com>
2023-09-10 19:21:39 +0200

    start add filters in champions page

--- src/pages/Champions.tsx
+++ src/pages/Champions.tsx
@@ -3,25 +3,40 @@ import { useLoaderData } from "react-router-dom";
 import { getChampionsUrl } from "../utils/urlUtils";
 
 import classes from "./ChampionsPage.module.css";
+import Tag from "../layout/Tag";
+
+const tags = ["Mage", "Tank", "Fighter", "Assassin", "Marksman", "Support"];
 
 export default function ChampionsPage() {
   const data: any = useLoaderData();
   const champions = data && data.champions;
   return (
-    <div className={classes["champions-container"]}>
-      {champions &&
-        champions.map((champion: any) => (
-          <ChampionsGeneric
-            key={champion.key}
-            id={champion.id}
-            info={champion.info}
-            name={champion.name}
-            title={champion.title}
-            blurb={champion.blurb}
-            tags={champion.tags}
-          />
-        ))}
-    </div>
+    <>
+      <div className={classes.filters}>
+        <div className={classes["tags-container"]}>
+          {tags.map((tag) => (
+            <Tag key={tag} tagType={tag}>
+              {tag}
+            </Tag>
+          ))}
+        </div>
+      </div>
+
+      <div className={classes["champions-container"]}>
+        {champions &&
+          champions.map((champion: any) => (
+            <ChampionsGeneric
+              key={champion.key}
+              id={champion.id}
+              info={champion.info}
+              name={champion.name}
+              title={champion.title}
+              blurb={champion.blurb}
+              tags={champion.tags}
+            />
+          ))}
+      </div>
+    </>
   );
 }
 
