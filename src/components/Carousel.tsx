704d1923fbaf6838cd3678bafb79d318b3e2b5cf
francesco-orlando9 <francesco.orlando.dev@gmail.com>
2023-09-22 21:51:48 +0200

    refactor carousel + add transition

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,10 +1,4 @@
 import { useCallback, useEffect, useState } from "react";
-import {
-  AnimatePresence,
-  motion,
-  useDragControls,
-  useScroll,
-} from "framer-motion";
 
 import classes from "./Carousel.module.css";
 import Card from "../layout/Card";
@@ -13,52 +7,91 @@ interface CarouselProps {
   components: any[];
 }
 
+interface SwipeState {
+  isSwiping: boolean;
+  swipeDistance: number;
+  shouldTransition: boolean;
+}
+
+enum DIRECTION {
+  LEFT = "left",
+  RIGHT = "right",
+  NONE = "none",
+}
+
+const defaultSwipeState: SwipeState = {
+  isSwiping: false,
+  swipeDistance: 0,
+  shouldTransition: true,
+};
+
 const Carousel = ({ components }: CarouselProps) => {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
-  const [touchStart, setTouchStart] = useState<number | null>(null);
-  const [touchEnd, setTouchEnd] = useState<number | null>(null);
-  const [shouldTransition, setShouldTransition] = useState<boolean>(false);
+  const [touchStart, setTouchStart] = useState<number>(0);
+  const [touchEnd, setTouchEnd] = useState<number>(0);
+
+  const [swipeState, setSwipeState] = useState<SwipeState>(defaultSwipeState);
 
   const minSwipeDistance = 50;
 
   useEffect(() => {
     const interval = setInterval(() => {
-      if (!touchEnd) {
+      if (!swipeState.isSwiping) {
         simulateSlide();
       }
     }, 10000);
-    // simulateSlide();
 
     return () => {
       clearInterval(interval);
     };
-  }, [touchEnd]);
+  }, [swipeState]);
 
   const onTouchStart = (e: any) => {
-    setTouchEnd(null);
+    setTouchEnd(0);
     setTouchStart(e.targetTouches[0].clientX);
   };
 
   const onTouchMove = (e: any) => {
+    setSwipeState({
+      isSwiping: true,
+      swipeDistance: touchStart - e.targetTouches[0].clientX,
+      shouldTransition: false,
+    });
     setTouchEnd(e.targetTouches[0].clientX);
   };
 
   const onTouchEnd = () => {
+    setSwipeState({
+      isSwiping: false,
+      swipeDistance: 0,
+      shouldTransition: true,
+    });
     if (!touchStart || !touchEnd) return;
-    // setShouldTransition(true);
-    const distance = touchStart - touchEnd;
-    const isLeftSwipe = distance > minSwipeDistance;
-    const isRightSwipe = distance < -minSwipeDistance;
-    if (isLeftSwipe) {
+
+    const direction = getSwipeDirection();
+
+    if (direction === DIRECTION.LEFT) {
       handleNext();
     }
-    if (isRightSwipe) {
+    if (direction === DIRECTION.RIGHT) {
       handlePrevious();
     }
 
-    setTouchStart(null);
-    setTouchEnd(null);
-    // setTimeout(() => setShouldTransition(false), 600);
+    setTouchStart(0);
+    setTouchEnd(0);
+  };
+
+  const getSwipeDirection = (): DIRECTION => {
+    let direction = DIRECTION.NONE;
+    const distance = touchStart - touchEnd;
+    if (distance > minSwipeDistance) {
+      direction = DIRECTION.LEFT;
+    }
+    if (distance < -minSwipeDistance) {
+      direction = DIRECTION.RIGHT;
+    }
+
+    return direction;
   };
 
   const carouselComponents = [components[0], ...components[1]];
@@ -80,14 +113,20 @@ const Carousel = ({ components }: CarouselProps) => {
   }, []);
 
   const simulateSlide = () => {
-    setShouldTransition(true);
-    setTouchEnd(5);
-    setTouchStart(150);
-    setTimeout(() => {
-      setTouchEnd(null);
-      setTouchStart(null);
-    }, 300);
-    setTimeout(() => setShouldTransition(false), 600);
+    setSwipeState({
+      isSwiping: true,
+      swipeDistance: 150,
+      shouldTransition: true,
+    });
+    setTimeout(
+      () =>
+        setSwipeState({
+          isSwiping: false,
+          swipeDistance: 0,
+          shouldTransition: true,
+        }),
+      300
+    );
   };
 
   return (
@@ -98,43 +137,24 @@ const Carousel = ({ components }: CarouselProps) => {
       onTouchEnd={onTouchEnd}
     >
       <div style={{ position: "relative", height: 215, overflow: "hidden" }}>
-        <div
-          style={{
-            transition: shouldTransition ? "transform 0.3s" : "",
-            transform: `translateX(-${
-              touchEnd && touchStart ? 350 - touchEnd + touchStart : 350
-            }px)`,
-            position: "absolute",
-          }}
-        >
-          <Card>
-            {carouselComponents[currentIndex === 0 ? 4 : currentIndex - 1]}
-          </Card>
-        </div>
-        <div
-          style={{
-            transition: shouldTransition ? "transform 0.3s" : "",
-            transform: `translateX(${
-              touchEnd && touchStart ? touchEnd - touchStart : 0
-            }px)`,
-            position: "absolute",
-          }}
-        >
-          <Card>{carouselComponents[currentIndex]}</Card>
-        </div>
-        <div
-          style={{
-            transition: shouldTransition ? "transform 0.3s" : "",
-            transform: `translateX(${
-              touchEnd && touchStart ? touchEnd - touchStart + 350 : 350
-            }px)`,
-            position: "absolute",
-          }}
-        >
-          <Card>
-            {carouselComponents[currentIndex === 4 ? 0 : currentIndex + 1]}
-          </Card>
-        </div>
+        {carouselComponents.map((component, index) => (
+          <div
+            key={index}
+            style={{
+              position: "absolute",
+              transform: `translateX(${
+                currentIndex === 0 && !swipeState.isSwiping
+                  ? index * 350
+                  : swipeState.isSwiping
+                  ? index * 350 - currentIndex * 350 - swipeState.swipeDistance
+                  : index * 350 - currentIndex * 350
+              }px)`,
+              transition: swipeState.shouldTransition ? "transform 0.3s" : "",
+            }}
+          >
+            <Card>{component}</Card>
+          </div>
+        ))}
       </div>
 
       <div className={classes["carousel-indicator"]}>
